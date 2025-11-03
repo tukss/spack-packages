e2b0139300f89f026438e39bd67e4d396d25c871
Philipp Edelmann <pedelmann@lanl.gov>
2025-11-03 13:58:30 -0700

    new versions

--- repos/spack_repo/builtin/packages/codipack/package.py
+++ repos/spack_repo/builtin/packages/codipack/package.py
@@ -19,6 +19,10 @@ class Codipack(CMakePackage, Package):
     url = "https://github.com/SciCompKL/CoDiPack/archive/refs/tags/v2.1.0.tar.gz"
     git = "https://github.com/SciCompKL/CoDiPack.git"
 
+    version("3.0.0", sha256="cc25ae436118d1347f8efa7fd0df4c0af00b48e910133ecdbce97968084a9106")
+    version("2.3.2", sha256="82efa259139b95ce1f1447e6f8180b139f61e7b361e9c1bcd36a78feaab099ca")
+    version("2.3.1", sha256="c36821f31da9cc0c5b3e9e6c398cc18337db0117a023364781f9a2289aa69e28")
+    version("2.3.0", sha256="43f976e23a6a77de7e858e474159026ebe93d480274ab389721b8cf78d9124cc")
     version("2.2.0", sha256="24e9129829588fd8965620f275e40ae3a0be3b24015bc7d7280fa5ad551c10ac")
     version("2.1.0", sha256="c8d07eb01eaa056175902d5b153b8606b05d208ff0a541d15284f4d9ff6e87c2")
     version("2.0.2", sha256="c6eecfdbf5818daf80871461f23f8a29b5b72e314d2034047d0b0fcd44744339")
--- repos/spack_repo/builtin/packages/medipack/package.py
+++ repos/spack_repo/builtin/packages/medipack/package.py
@@ -16,6 +16,9 @@ class Medipack(CMakePackage):
 
     maintainers("justinh2002")
 
+    version("1.3.3", sha256="e56a3cf93379ebdacb0778130e7476a779f2872978bdbab2471a9072ae8e23f0")
+    version("1.3.2", sha256="b5d307eac485fb23738dd1574572839a0e70ec38578d3d8eba471605a461c84b")
+    version("1.3.1", sha256="8b9841de207ee798fd8fc7cdc315917d0e2695ba39798cb5af7d18186cf862c8")
     version("1.3.0", sha256="81daf8391ca00286a1276408badc7f1c9f76af889eb16940601c0ffb5f229e1d")
     version("1.2.2", sha256="8937fa1025c6fb12f516cacf38a7f776221e7e818b30f17ce334c63f78513aa7")
     version("1.2.1", sha256="c746196b98cfe24a212584cdb88bd12ebb14f4a54728070d605e0c6d0e75db8a")
